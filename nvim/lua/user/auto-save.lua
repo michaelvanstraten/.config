186ec1c401ff9889ab4d9b74e6ccfc3a24b613b5
Michael van Straten <michael@vanstraten.de>
2023-02-18 16:03:55 +0100

    removed wireshark configs

--- nvim/lua/user/auto-save.lua
+++ nvim/lua/user/auto-save.lua
@@ -1,9 +1,9 @@
 -- Use a protected call so we don"t error out on first use
 local auto_save_require_ok, auto_save = pcall(require, "auto-save")
 if not auto_save_require_ok then
-	return
+    return
 end
 
 auto_save.setup({
-	trigger_events = { "TextChanged", "TextChangedI" },
+    trigger_events = { "TextChanged", "TextChangedI" },
 })
